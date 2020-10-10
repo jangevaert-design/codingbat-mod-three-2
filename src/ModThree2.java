e973fb8c5ed4d7e862436d64068940f645f1538c
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-10 12:58:04 -0600

    Initial commit

---
+++ src/ModThree2.java
@@ -0,0 +1,12 @@
+public class ModThree2 {
+  public boolean modThree(int[] nums) {
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      if ((nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) ||
+          (nums[i] % 2 != 0 && nums[i + 1] % 2 != 0 && nums[i + 2] % 2 != 0)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
